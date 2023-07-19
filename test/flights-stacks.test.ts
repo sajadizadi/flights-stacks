836dcb2d4a168f6c167064f361bce7ef651d7faf
sajadizadi <sajad.izadi@bmo.com>
2023-07-19 14:43:50 -0400

    Initial commit

---
+++ test/flights-stacks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FlightsStacks from '../lib/flights-stacks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flights-stacks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlightsStacks.FlightsStacksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
